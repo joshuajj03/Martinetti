b8aa4a3afdacc036c83d42e369d5ebf7a41a2604
Joshua Jacobson <21joshua.jacobson@boiseschools.net>
2019-08-23 12:54:48 -0600

    Added Stuff

--- Martinetti.py
+++ Martinetti.py
@@ -19,3 +19,52 @@ class Die :
         import random
         self.num = random.randint(1,self.sides)
     
+
+# Class that tracks the position of players on the gameboard and moves them
+class Board:
+
+    def __init__ ( self ) :
+        self.boardList = [1,2,3,4,5,6,7,8,9,10,11,12,11,10,9,8,7,6,5,4,3,2,1]
+        self.boardPos = 0
+
+    def __str__ ( self ) :
+        return "Board Position: " + str(self.boardPos)
+    def moveUp( self, num1, num2, num3) :
+        tracker = self.boardPos - 1
+        while( tracker < self.boardPos) :
+            tracker = self.boardPos
+            if( num1 == self.boardList[self.boardPos]) :
+                self.boardPos += 1
+            else if( num2 == self.boardList[self.boardPos]) :
+                self.boardPos += 1
+            else if( num3 == self.boardList[self.boardPos]) :
+                self.boardPos += 1
+            else if( num1 + num2 == self.boardList[self.boardPos]) :
+                self.boardPos += 1
+            else if( num1 + num3 == self.boardList[self.boardPos]) :
+                self.boardPos += 1
+            else if( num2 + num3 == self.boardList[self.boardPos]) :
+                self.boardPos += 1
+            else if( num1 + num2 + num3 == self.boardList[self.boardPos]) :
+                self.boardPos += 1
+            if( self.boardPos == 11 or self.boardPos == 23) :
+                break
+        return self.boardPos
+
+class Player:
+
+    def __init__( self, playerName ) :
+        gameBoard = Board()
+        self.name = playerName
+
+    def __str__( self ) :
+        return playerName
+
+    def hasWon( self ) :
+        if( self.gameBoard.boardPos == 23):
+            return True
+        return False
+        
+            
+class Game:
+    
